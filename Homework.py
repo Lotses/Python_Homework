b7cc70b8adba3d89924c105c8f2f93ae21186ce9
Lotses <Lotses@mail.ru>
2022-05-13 09:33:30 +0300

    второе задание

--- Homework.py
+++ Homework.py
@@ -1,6 +1,27 @@
-# Задание 1
-cat_name = input('Введите имя кота: ')
-expenses = input('Введите сколько рублей кот тратит на корм: ')
-print(f'Ваш кот {cat_name.capitalize()} очень наглый, он тратит аж {expenses} рублей на корм')
+##_____________________ Задание 1
+# print('Задание №1 ↓')
+# cat_name = input('Введите имя кота: ')
+# expenses = input('Введите сколько рублей кот тратит на корм: ')
+# print(f'Ваш кот {cat_name.capitalize()} очень наглый, он тратит аж {expenses} рублей на корм')
 
-# Задание 2
+##_____________________ Задание 2
+# class DateTime(): # Создаём класс и функцию для вывода в формате чч:мм:сс (если значение меньше 10, то выведется 0Ч:0М:0С)
+#     def __init__(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#
+#     def display(self):
+#         print(f'Введённое количество секунд соответствует {self.hours:02}:{self.minutes:02}:{self.seconds:02}')
+#
+# print('Задание №2 ↓')
+# seconds = int(input('Введите количество секунд: '))
+# hours = seconds // 3600
+# minutes = (seconds % 3600) // 60
+# sec = (seconds % 3600) % 60
+#
+# time_1 = DateTime(hours, minutes, sec)
+# time_1.display()
+
+#_____________________ Задание 3
+ print('Задание №1 3')
